f80b7a2c3e4acb20691554cd258f1d40fd83e752
Saulo <saulo.mendes@bsd.com.br>
2025-03-19 20:32:06 -0300

    feat: Inclusao da camada de controller

---
+++ src/main/java/br/com/teste/credito/infraestructure/controller/CreditoController.java
@@ -0,0 +1,39 @@
+package br.com.teste.credito.infraestructure.controller;
+
+import br.com.teste.credito.application.service.CreditoService;
+import br.com.teste.credito.infraestructure.controller.dto.CreditoResponse;
+import br.com.teste.credito.infraestructure.controller.mapper.CreditoMapper;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("v1/api/creditos")
+public class CreditoController {
+
+    private final CreditoService service;
+
+    public CreditoController(CreditoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{numeroNfse}")
+    public List<CreditoResponse> consultarPorNfse(@PathVariable String numeroNfse) {
+        log.info("Consultando uma lista de creditos pela NFSE: {}", numeroNfse);
+
+        return service.buscarPorNfse(numeroNfse)
+                .stream().map(CreditoMapper::toResponse).toList();
+    }
+
+    @GetMapping("/{numeroCredito}/credito")
+    public CreditoResponse consultarPorNumeroCredito(@PathVariable String numeroCredito) {
+        log.info("Consulta de credito pelo numeroCredito: {}", numeroCredito);
+
+        var credito = service.buscarPorNumeroCredito(numeroCredito);
+        return CreditoMapper.toResponse(credito);
+    }
+}
---
+++ src/main/java/br/com/teste/credito/infraestructure/exception/CustomExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.com.teste.credito.infraestructure.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(500, ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFoundException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(404, ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
