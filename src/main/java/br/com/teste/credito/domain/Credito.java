7a1e085c3d312ecceef12e231da14424ee908abe
Saulo <saulo.mendes@bsd.com.br>
2025-03-19 19:40:33 -0300

    feat: Configuração e inclusao das classes de dominio e persistencia

---
+++ src/main/java/br/com/teste/credito/domain/Credito.java
@@ -0,0 +1,35 @@
+package br.com.teste.credito.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@Entity
+@Table(name = "credito")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Credito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numeroCredito;
+    private String numeroNfse;
+    private LocalDate dataConstituicao;
+    private BigDecimal valorIssqn;
+    private String tipoCredito;
+    private Boolean simplesNacional;
+    private BigDecimal aliquota;
+    private BigDecimal valorFaturado;
+    private BigDecimal valorDeducao;
+    private BigDecimal baseCalculo;
+}
---
+++ src/main/java/br/com/teste/credito/infraestructure/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package br.com.teste.credito.infraestructure.exception;
+
+public class ErrorResponse {
+    private Integer statusCode;
+    private String message;
+
+    public ErrorResponse(Integer statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
