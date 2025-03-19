a6efedbeb3532a6baab48cc78a76dc8cf5bec5a8
Saulo <saulo.mendes@bsd.com.br>
2025-03-19 20:44:07 -0300

    feat: Ajuste de pacote de configuracao do handler

--- src/main/java/br/com/teste/credito/infraestructure/config/CustomExceptionHandler.java
+++ src/main/java/br/com/teste/credito/infraestructure/config/CustomExceptionHandler.java
@@ -1,5 +1,7 @@
-package br.com.teste.credito.infraestructure.exception;
+package br.com.teste.credito.infraestructure.config;
 
+import br.com.teste.credito.infraestructure.exception.ErrorResponse;
+import br.com.teste.credito.infraestructure.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
